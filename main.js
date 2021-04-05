a7203fbdae12e29b6b30c2b1fe8344f16766bd3a
giancode1 <gieg5c@gmail.com>
2021-04-04 19:49:24 -0500

    Primer commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function printTime() {
+    var x = new Date();
+
+    var day = x.getDate(),
+	month = x.getMonth(),
+	year = x.getFullYear(),
+    fechaActual = document.getElementById("fecha"),
+
+    hour = (x.getHours()<10 ? "0":"") + x.getHours(),
+    min = (x.getMinutes()<10 ? "0":"") + x.getMinutes(),
+    sec = (x.getSeconds()<10 ? "0":"") + x.getSeconds(),
+    horaActual = document.getElementById("hora");
+
+    horaActual.innerHTML = hour + ":" + min +":" + sec;
+
+    var dias = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+    var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    
+    var mesActual = meses[month];
+    var diaActual = dias[day];
+    fechaActual = document.getElementById("fecha");
+    fechaActual.innerHTML = `${diaActual} ${day} de ${mesActual} del ${year}`;
+}
+setInterval(printTime,1000);
+
